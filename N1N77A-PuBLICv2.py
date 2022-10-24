8c66e60ed9b325882fb694692fbc6d3ce7db5824
N1N77A <113140014+N1N77A@users.noreply.github.com>
2022-10-24 20:25:37 +0600

    Add files via upload

---
+++ N1N77A-PuBLICv2.py
@@ -0,0 +1,25 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+
+        os.system('xdg-open https://www.facebook.com/KAyNAT.BabY')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from N1N77A-V2 import Subscription
+ 
+        Subscription()
+ 
+        
